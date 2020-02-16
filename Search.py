cb903fe8f87509f209defaeecdf6de9f28aaf052
Yashwanth Soodini <ysoodini@gmail.com>
2020-02-15 21:07:30 -0500

    Define HashQueue and HashPriorityQueue classes

--- Search.py
+++ Search.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from heapq import heappush, heappop
 class Problem(ABC):
     """ Abstract class for a Search Problem.
 
@@ -20,7 +21,6 @@ class Problem(ABC):
         Returns:
             A list of actions
         """
-        pass
 
     @abstractmethod
     def result(self, state, action):
@@ -33,7 +33,6 @@ class Problem(ABC):
         Returns:
             Resulting state
         """
-        pass
 
     @abstractmethod
     def goal_test(self, state):
@@ -45,7 +44,6 @@ class Problem(ABC):
         Returns:
             Boolean indicating whether or not the given state is a goal state
         """
-        pass
 
     @abstractmethod
     def step_cost(self, state, action, res):
@@ -59,45 +57,45 @@ class Problem(ABC):
         Returns:
             An instance of a numeric data type indicating the step cost.
         """
-        pass
 
 class ShortestPathProblem(Problem):
     """ Shortest Path Problem.
     
-    This class represents the problem of finding the shortest path between two cities.
+    This class represents the problem of finding the shortest path between two 
+    cities.
     Implements the Abstract class 'Problem'.
 
     An environment state in this problem is just the name of a city.
+
+    Attributes:
+        roadmap: A dict that indicates adjacent cities of each city and their 
+            distances
+            For example:
+            
+            {'austin': [('houston',186), ('sanAntonio',79)],
+             'buffalo': [('toronto', 105), ('rochester', 64), ('cleveland', 19)]
+             'dallas': [('denver', 792), ('mexia', 83)]}
+                
+        initial_state: The city agent is currently located at.
+            Example:
+            
+            'portlandOR'
+
+        goal_state: The destination city.
+            Example:
+
+            'bostonMA'
     """
 
-    def __init__(self, map, initial_state, goal_state):
-        """ Init the problem with a road map and initial state
-        
-        Args:
-            map: A dict that indicates adjacent cities of each city and their distances
-                For example:
-                
-                {'austin': [('houston',186), ('sanAntonio',79)],
-                 'buffalo': [('toronto', 105), ('rochester', 64), ('cleveland', 191)]
-                 'dallas': [('denver', 792), ('mexia', 83)]}
-                 
-            initial_state: The city agent is currently located at.
-                Example:
-                
-                'portlandOR'
-
-            goal_state: The destination city.
-                Example:
-
-                'bostonMA'
-        """
-        self.map = map
+    def __init__(self, roadmap, initial_state, goal_state):
+        """ Init the ShortestPathProblem with the provided attributes"""
+        self.roadmap = roadmap
         self.initial_state = initial_state
         self.goal_state = goal_state
 
     def actions(self, state):
         """ Returns cities reachable from 'state' """
-        return [city for city,_ in self.map[state]]
+        return [city for city, _ in self.roadmap[state]]
 
     def result(self, state, action):
         """ The resulting state from performing 'action' in city 'state'
@@ -113,12 +111,14 @@ class ShortestPathProblem(Problem):
         return action
 
     def goal_test(self, state):
-        """ Returns boolean indicating whether given 'state' is the destination """
+        """ Returns boolean indicating whether given 'state' is the 
+            destination 
+        """
         return (state == self.goal_test)
 
     def step_cost(self, state, action, res):
         """ Returns the distance from city 'state' to city 'res' """
-        return [dist for city,dist in self.map[state] if city == state ]
+        return [dist for city, dist in self.roadmap[state] if city == state]
 
 class Node:
     """ A class to represent a node in a search tree.
@@ -130,9 +130,95 @@ class Node:
         path_cost: the cost of the path from the initial state to the node
     """
 
-    def __init__(self,state,parent,action,path_cost):
+    def __init__(self, state, parent, action, path_cost):
         """ Inits the Node with the provided attributes """
         self.state = state
         self.parent = parent
         self.action = action
         self.path_cost = path_cost
+
+def child_node(problem, parent, action):
+    """ Given parent node, child node.
+    
+    Takes a parent node and an action and returns the resulting child node.
+
+    Args:
+        problem: the problem being solved by the search algorithm/agent
+        parent: the parent node
+        action: the action performed on the parent node
+
+    Returns:
+        the resulting child node
+    """
+    
+    if problem is not Problem:
+        raise TypeError("First argument must implement abstract class Problem")
+    state = problem.result(parent.state, action)
+    path_cost = parent.path_cost + problem.step_cost(parent.state, action)
+    child = Node(state, parent, action, path_cost)
+    return child
+
+class HashQueue:
+    """ A data structure that combines the capabilities of a queue and a hash 
+        table. 
+    """
+
+    def __init__(self):
+        """ Init a set to support efficient membership testing and a list that 
+            implements a queue
+        """
+        self.hash = {}
+        self.queue = []
+
+    def is_empty(self):
+        """ Returns boolean indicating whether or not queue is empty """
+        return not self.queue
+
+    def pop(self):
+        """ Returns the earliest inserted node among current nodes """
+        qhead = self.queue.pop(0)
+        self.hash.remove(qhead.state)
+        return qhead
+
+    def insert(self, node):
+        """ Inserts a node to the end of the queue """
+        self.queue.append(node)
+        self.hash.add(node.state)
+
+    def contains(self, state):
+        """ Returns whether or not a state is already in the queue """
+        return state in self.hash
+
+class HashPriorityQueue:
+    """ A data structure that combines the capabilities of a priority queue and 
+        a hash map. 
+    """
+    
+    def __init__(self):
+        """ Init a dict to support efficient membership testing and a list that 
+            implements a priority queue
+        """
+        self.hash = {}
+        self.queue = []
+
+    def is_empty(self):
+        """ Returns boolean indicating whether or not queue is empty """
+        return not self.queue
+
+    def pop(self):
+        """ Returns node with highest priority """
+        negative_priority, node = heappop(self.queue)
+        del self.hash[node.state]
+        return node
+
+    def insert(self, node, priority):
+        """ inserts a node into the queue according to its priority """
+        heappush(self.queue, (-priority, node)) #negative priority since minheap
+        self.hash[node.state] = node.path_cost
+
+    def contains(self, state):
+        """ Returns whether or not a state is already in the queue """
+        return state in self.hash
+
+if __name__ == "__main__" do:
+    
